d102c120d154b3ef1d7093e9e58422b4f378cd24
theQuazz <djrquazz@gmail.com>
2013-05-10 00:56:32 -0400

    Be more precies about what files to take

--- run-spec.rkt
+++ run-spec.rkt
@@ -1,12 +1,19 @@
 #lang racket
 
-(define (load-spec file) (dynamic-require file #f))
+(define (output-to-console file)
+        (define name (some-system-path->string file))
+        (display (format "\n--Running ~a...\n\n" name))
+        (dynamic-require file #f)
+        (display (format "\n--Finished running ~a...\n\n" name)))
+
+(define (is-a-test-file file)
+        (define name (some-system-path->string file))
+        (and (file-exists? file)
+             (bytes=? (filename-extension file) #"rkt")
+             (>= (string-length name) 5)
+             (string=? (substring name 0 5) "test-")))
 
 (for-each (lambda (file)
-            (define name (some-system-path->string file))
-            (cond [(string=? (substring name 0 5) "test-")
-                   (and (display (format "\n--Running ~a...\n\n" name))
-                        (load-spec file)
-                        (display (format "\n--Finished running ~a...\n\n" name)))]))
+            (if (is-a-test-file file) (output-to-console file) '()))
           (directory-list))
 
